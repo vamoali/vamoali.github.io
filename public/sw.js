f07428ecdfef376cb6004eb0f7f4f5d1c83864d8
Paulo Roberto <paulo.pinda@gmail.com>
2026-02-05 22:35:37 -0300

    fixes page.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "vamoali-pwa-v1";
+const CACHE_NAME = "vamoali-pwa-v2";
 const CORE_ASSETS = ["/", "/manifest.json", "/images/icon.svg"];
 
 self.addEventListener("install", (event) => {
